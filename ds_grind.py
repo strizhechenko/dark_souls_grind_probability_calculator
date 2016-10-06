9021836bebc66b48b26e15f0454301937147ae24
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-10-07 01:53:32 +0500

    Рабочий просчитывальщик вероятности удачи при гринде в Dark Souls 2

---
+++ ds_grind.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+# pylint: disable=C0111
+
+from dictator import Dictator
+
+
+def case_count(result, _try):
+    return float(sum(int(result.get(x, 0)) for x in result.keys() if int(x) == _try))
+
+
+def case_count_greater(result, _try):
+    return float(sum(int(result.get(x, 0)) for x in result.keys() if int(x) >= _try))
+
+
+def main():
+    _try = 1
+    result = Dictator(db=2)
+    while True:
+        probability = case_count(result, _try) / (case_count_greater(result, _try) or 0.00000001)
+        print "Try {0}. Probability {1}. Respawned? y/N".format(_try, probability),
+        answer = raw_input()
+        if answer.lower() != 'y':
+            _try += 1
+            continue
+        result[_try] = int(result.get(_try, 0)) + 1
+        _try = 1
+
+
+if __name__ == '__main__':
+    main()

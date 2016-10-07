b6f2e31066b4820f085219046b5340787fde12e6
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2016-10-07 10:58:42 +0500

    Заменил две одинаковых функции использованием operator

--- ds_grind.py
+++ ds_grind.py
@@ -2,23 +2,20 @@
 # coding: utf-8
 # pylint: disable=C0111
 
+from operator import eq, ge
 from os import getenv
 from dictator import Dictator
 
 
-def case_count(result, _try):
-    return float(sum(int(result.get(x, 0)) for x in result.keys() if int(x) == _try))
-
-
-def case_count_greater(result, _try):
-    return float(sum(int(result.get(x, 0)) for x in result.keys() if int(x) >= _try))
+def case_count(result, _try, op):
+    return float(sum(int(v) for k, v in result.items() if op(int(k), _try)))
 
 
 def main():
     _try = 1
     result = Dictator(db=int(getenv('redis_db', '2')))
     while True:
-        probability = case_count(result, _try) / (case_count_greater(result, _try) or 0.00000001)
+        probability = case_count(result, _try, eq) / (case_count(result, _try, ge) or 0.00000001)
         print "Try {0}. Probability {1}. Respawned? y/N".format(_try, probability),
         answer = raw_input()
         if answer.lower() != 'y':
